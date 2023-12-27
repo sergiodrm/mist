808408dee939b297138a2a9b084c02d8ec078d61
sergiodrm <sergiojg.drm@gmail.com>
2023-12-27 23:41:46 +0100

    script cpp template

--- scripts/NewSrcFile.bat
+++ scripts/NewSrcFile.bat
@@ -8,8 +8,8 @@ set src_file=%~2
 set header_path=%currdir%%code_rel_dir%%header_file%
 set src_path=%currdir%%code_rel_dir%%src_file%
 
-echo // header file for vkmmc project > %header_path%
-echo // src file for vkmmc project > %src_path%
+cat header_template.h.tpl > %header_path%
+cat src_template.cpp.tpl > %src_path%
 
 echo Running cmake...
 cd ../build
