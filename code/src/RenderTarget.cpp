e737eb4ea5c7c70cde733ce0a4ca366038e11747
sergiodrm <sergiojg.drm@gmail.com>
2024-03-30 16:00:35 +0100

    render target wip

---
+++ code/src/RenderTarget.cpp
@@ -0,0 +1,249 @@
+#include "RenderTarget.h"
+#include "Debug.h"
+#include "RenderContext.h"
+#include "InitVulkanTypes.h"
+
+namespace vkmmc
+{
+	VkAttachmentDescription GetVkDescription(const RenderTargetAttachmentDescription& desc)
+	{
+		return vkinit::RenderPassAttachmentDescription(desc.Format, desc.Layout);
+	}
+
+	void RenderTargetAttachment::Destroy(const RenderContext& renderContext)
+	{
+		vkDestroyImageView(renderContext.Device, View, nullptr);
+		vkDestroyImage(renderContext.Device, Image.Image, nullptr);
+	}
+
+	RenderTarget::RenderTarget()
+		:
+#ifdef VKMMC_VULKAN
+		m_renderPass(VK_NULL_HANDLE),
+		m_framebuffer(VK_NULL_HANDLE)
+#endif // VKMMC_VULKAN
+	{
+	}
+
+	RenderTarget::~RenderTarget()
+	{
+		check(m_renderPass == VK_NULL_HANDLE);
+		check(m_framebuffer == VK_NULL_HANDLE);
+	}
+
+	void RenderTarget::Create(const RenderContext& renderContext, const RenderTargetDescription& desc)
+	{
+		m_description = desc;
+		CreateResources(renderContext);
+	}
+
+	void RenderTarget::Destroy(const RenderContext& renderContext)
+	{
+		check(m_renderPass != VK_NULL_HANDLE);
+		check(m_framebuffer != VK_NULL_HANDLE);
+		for (uint32_t i = 0; i < (uint32_t)m_description.ColorAttachmentCount; ++i)
+		{
+			m_attachments[i].Destroy(renderContext);
+		}
+		if (m_description.DepthAttachmentDescription.IsValidAttachment())
+		{
+			m_attachments[m_description.ColorAttachmentCount].Destroy(renderContext);
+		}
+
+		vkDestroyFramebuffer(renderContext.Device, m_framebuffer, nullptr);
+		m_framebuffer = VK_NULL_HANDLE;
+		vkDestroyRenderPass(renderContext.Device, m_renderPass, nullptr);
+		m_renderPass = VK_NULL_HANDLE;
+	}
+
+	void RenderTarget::Invalidate(const RenderContext& renderContext)
+	{
+		Destroy(renderContext);
+		CreateResources(renderContext);
+	}
+
+	void RenderTarget::Bind(VkCommandBuffer cmd)
+	{
+		VkRenderPassBeginInfo renderPassInfo = { VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO, nullptr };
+		renderPassInfo.renderPass = m_renderPass;
+		renderPassInfo.renderArea = m_description.RenderArea;
+		renderPassInfo.framebuffer = m_framebuffer;
+		renderPassInfo.clearValueCount = (uint32_t)m_clearValues.size();
+		renderPassInfo.pClearValues = m_clearValues.data();
+		vkCmdBeginRenderPass(cmd, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+	}
+
+	uint32_t RenderTarget::GetWidth() const
+	{
+		return m_description.RenderArea.extent.width;
+	}
+
+	uint32_t RenderTarget::GetHeight() const
+	{
+		return m_description.RenderArea.extent.height;
+	}
+
+	uint32_t RenderTarget::GetAttachmentCount() const
+	{
+		return (uint32_t)m_attachments.size();
+	}
+
+	const RenderTargetAttachment& RenderTarget::GetAttachment(uint32_t index) const
+	{
+		check(index < GetAttachmentCount());
+		return m_attachments[index];
+	}
+
+	const RenderTargetDescription& RenderTarget::GetDescription() const
+	{
+		return m_description;
+	}
+
+	void RenderTarget::CreateResources(const RenderContext& renderContext)
+	{
+		check(m_renderPass == VK_NULL_HANDLE);
+		check(m_framebuffer == VK_NULL_HANDLE);
+		CreateRenderPass(renderContext);
+		if (m_description.ExternalFramebuffer == VK_NULL_HANDLE)
+			CreateFramebuffer(renderContext);
+	}
+
+	void RenderTarget::CreateRenderPass(const RenderContext& renderContext)
+	{
+		// Create render pass
+		check(m_description.ColorAttachmentCount <= MAX_RENDER_TARGET_COLOR_ATTACHMENTS);
+		tArray<VkAttachmentDescription, MAX_RENDER_TARGET_ATTACHMENTS> attachments;
+		// TODO: do we need to support multiple subpasses inside a RenderPass?
+		tArray<VkAttachmentReference, MAX_RENDER_TARGET_ATTACHMENTS> attachmentReferences;
+		for (uint32_t i = 0; i < m_description.ColorAttachmentCount;++i)
+		{
+			// Fill attachment info
+			attachments[i] = GetVkDescription(m_description.ColorAttachmentDescriptions[i]);
+			// References
+			attachmentReferences[i].attachment = i;
+			attachmentReferences[i].layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+		}
+
+		// Depth attachment
+		uint32_t attachmentCount = m_description.ColorAttachmentCount;
+		VkAttachmentReference* depthReference = VK_NULL_HANDLE;
+		if (m_description.DepthAttachmentDescription.IsValidAttachment())
+		{
+			uint32_t depthAttachmentIndex = m_description.ColorAttachmentCount;
+			attachments[depthAttachmentIndex] = GetVkDescription(m_description.DepthAttachmentDescription);
+			attachmentReferences[depthAttachmentIndex].attachment = depthAttachmentIndex;
+			attachmentReferences[depthAttachmentIndex].layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+			++attachmentCount;
+			depthReference = &attachmentReferences[depthAttachmentIndex];
+		}
+
+		VkSubpassDescription subpass;
+		subpass.flags = 0;
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = m_description.ColorAttachmentCount;
+		subpass.pColorAttachments = attachmentReferences.data();
+		subpass.pDepthStencilAttachment = depthReference;
+		// TODO: implement more type of attachments (input?, preserve and resolve)
+		subpass.inputAttachmentCount = 0;
+		subpass.pInputAttachments = nullptr;
+		subpass.preserveAttachmentCount = 0;
+		subpass.pPreserveAttachments = nullptr;
+		subpass.pResolveAttachments = nullptr;
+
+		// Create dependencies
+		tArray<VkSubpassDependency, 4> dependencies;
+		uint32_t dependencyCount = 0;
+		if (m_description.ColorAttachmentCount)
+		{
+			{
+				VkSubpassDependency& depedency = dependencies[dependencyCount++];
+				depedency.srcSubpass = VK_SUBPASS_EXTERNAL;
+				depedency.dstSubpass = 0;
+				depedency.srcStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+				depedency.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+				depedency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+				depedency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+				depedency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+			}
+			{
+				VkSubpassDependency& depedency = dependencies[dependencyCount++];
+				depedency.srcSubpass = 0;
+				depedency.dstSubpass = VK_SUBPASS_EXTERNAL;
+				depedency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+				depedency.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+				depedency.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+				depedency.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+				depedency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+			}
+		}
+
+		if (m_description.DepthAttachmentDescription.IsValidAttachment())
+		{
+			{
+				VkSubpassDependency& depedency = dependencies[dependencyCount++];
+				depedency.srcSubpass = VK_SUBPASS_EXTERNAL;
+				depedency.dstSubpass = 0;
+				depedency.srcStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+				depedency.dstStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+				depedency.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+				depedency.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+				depedency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+			}
+
+			{
+				VkSubpassDependency& depedency = dependencies[dependencyCount++];
+				depedency.srcSubpass = 0;
+				depedency.dstSubpass = VK_SUBPASS_EXTERNAL;
+				depedency.srcStageMask = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+				depedency.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+				depedency.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+				depedency.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+				depedency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+			}
+		}
+
+		VkRenderPassCreateInfo passCreateInfo{ .sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO, .pNext = nullptr };
+		passCreateInfo.flags = 0;
+		passCreateInfo.attachmentCount = attachmentCount;
+		passCreateInfo.pAttachments = attachments.data();
+		passCreateInfo.subpassCount = 1;
+		passCreateInfo.pSubpasses = &subpass;
+		passCreateInfo.dependencyCount = dependencyCount;
+		passCreateInfo.pDependencies = dependencies.data();
+		vkcheck(vkCreateRenderPass(renderContext.Device, &passCreateInfo, nullptr, &m_renderPass));
+	}
+
+	void RenderTarget::CreateFramebuffer(const RenderContext& renderContext)
+	{
+		tArray<VkImageView, MAX_RENDER_TARGET_ATTACHMENTS> views;
+		for (uint32_t i = 0; i < m_description.ColorAttachmentCount; ++i)
+		{
+			CreateAttachment(m_attachments[i], renderContext, m_description.ColorAttachmentDescriptions[i], IMAGE_ASPECT_COLOR_BIT, 
+				IMAGE_USAGE_COLOR_ATTACHMENT_BIT | IMAGE_USAGE_SAMPLED_BIT);
+			views[i] = m_attachments[i].View;
+		}
+		if (m_description.DepthAttachmentDescription.IsValidAttachment())
+		{
+			CreateAttachment(m_attachments[m_description.ColorAttachmentCount], renderContext, m_description.DepthAttachmentDescription, IMAGE_ASPECT_DEPTH_BIT,
+				IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | IMAGE_USAGE_SAMPLED_BIT);
+			views[m_description.ColorAttachmentCount] = m_attachments[m_description.ColorAttachmentCount].View;
+		}
+		VkFramebufferCreateInfo fbInfo = vkinit::FramebufferCreateInfo(m_renderPass,
+			m_description.RenderArea.extent.width,
+			m_description.RenderArea.extent.height,
+			views.data(), m_description.ColorAttachmentCount + 1
+		);
+		vkcheck(vkCreateFramebuffer(renderContext.Device, &fbInfo, nullptr, &m_framebuffer));
+	}
+
+	void RenderTarget::CreateAttachment(RenderTargetAttachment& attachment, const RenderContext& renderContext, const RenderTargetAttachmentDescription& description, EImageAspect aspect, EImageUsage imageUsage) const
+	{
+		tExtent3D extent{ .width = m_description.RenderArea.extent.width, .height = m_description.RenderArea.extent.height, .depth = 1 };
+		VkImageCreateInfo imageInfo = vkinit::ImageCreateInfo(description.Format, imageUsage, extent);
+		attachment.Image = Memory::CreateImage(renderContext.Allocator, imageInfo, MEMORY_USAGE_GPU);
+
+		VkImageViewCreateInfo viewInfo = vkinit::ImageViewCreateInfo(description.Format, attachment.Image.Image, aspect);
+		vkcheck(vkCreateImageView(renderContext.Device, &viewInfo, nullptr, &attachment.View));
+	}
+}
---
+++ code/src/RenderTarget.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include "RenderTypes.h"
+#include "Debug.h"
+
+#define MAX_RENDER_TARGET_COLOR_ATTACHMENTS 4
+#define MAX_RENDER_TARGET_ATTACHMENTS MAX_RENDER_TARGET_COLOR_ATTACHMENTS + 1 // Color + Depth
+
+namespace vkmmc
+{
+	struct RenderContext;
+
+	struct RenderTargetAttachmentDescription
+	{
+		ESampleCount MultisampledBit = SAMPLE_COUNT_1_BIT;
+		EFormat Format = FORMAT_UNDEFINED;
+		EImageLayout Layout = IMAGE_LAYOUT_UNDEFINED;
+		tClearValue ClearValue;
+
+		inline bool IsValidAttachment() const { return Format != FORMAT_UNDEFINED && Layout != IMAGE_LAYOUT_UNDEFINED; }
+	};
+
+	struct RenderTargetDescription
+	{
+		tRect2D RenderArea;
+		tArray<RenderTargetAttachmentDescription, MAX_RENDER_TARGET_COLOR_ATTACHMENTS> ColorAttachmentDescriptions;
+		uint32_t ColorAttachmentCount = 0;
+		RenderTargetAttachmentDescription DepthAttachmentDescription;
+#ifdef VKMMC_VULKAN
+		// Can exists an external fb with previous allocated image. Use this, but we have to create the render pass.
+		VkFramebuffer ExternalFramebuffer = VK_NULL_HANDLE;
+#endif // VKMMC_VULKAN
+
+		inline void AddColorAttachment(const RenderTargetAttachmentDescription& desc)
+		{
+			check(ColorAttachmentCount < MAX_RENDER_TARGET_COLOR_ATTACHMENTS);
+			ColorAttachmentDescriptions[ColorAttachmentCount++] = desc;
+		}
+
+		inline void AddColorAttachment(EFormat format, EImageLayout layout, ESampleCount sampleCount, tClearValue clearValue)
+		{
+			RenderTargetAttachmentDescription desc
+			{
+				.MultisampledBit = sampleCount,
+				.Format = format,
+				.Layout = layout,
+				.ClearValue = clearValue
+			};
+			AddColorAttachment(desc);
+		}
+
+	};
+
+	struct RenderTargetAttachment
+	{
+		AllocatedImage Image;
+#ifdef VKMMC_VULKAN
+		VkImageView View;
+#endif // VKMMC_VULKAN
+
+		void Destroy(const RenderContext& renderContext);
+	};
+
+	class RenderTarget
+	{
+	public:
+		RenderTarget();
+		~RenderTarget();
+
+		void Create(const RenderContext& renderContext, const RenderTargetDescription& desc);
+		void Destroy(const RenderContext& renderContext);
+		void Invalidate(const RenderContext& renderContext);
+
+		void Bind(VkCommandBuffer cmd);
+
+		uint32_t GetWidth() const;
+		uint32_t GetHeight() const;
+
+		inline VkFramebuffer GetFramebuffer() const { return m_framebuffer; }
+		inline VkRenderPass GetRenderPass() const { return m_renderPass; }
+		inline VkImageView GetRenderTarget(uint32_t index) const { return m_attachments[index].View; }
+
+		uint32_t GetAttachmentCount() const;
+		const RenderTargetAttachment& GetAttachment(uint32_t index) const;
+		const RenderTargetDescription& GetDescription() const;
+	protected:
+		void CreateResources(const RenderContext& renderContext);
+		void CreateRenderPass(const RenderContext& renderContext);
+		void CreateFramebuffer(const RenderContext& renderContext);
+		void CreateAttachment(RenderTargetAttachment& attachment, const RenderContext& renderContext, const RenderTargetAttachmentDescription& description, EImageAspect aspect, EImageUsage imageUsage) const;
+	private:
+#ifdef VKMMC_VULKAN
+		VkRenderPass m_renderPass;
+		VkFramebuffer m_framebuffer;
+		tArray<tClearValue, MAX_RENDER_TARGET_ATTACHMENTS> m_clearValues;
+#endif // VKMMC_VULKAN
+		RenderTargetDescription m_description;
+		tArray<RenderTargetAttachment, MAX_RENDER_TARGET_ATTACHMENTS> m_attachments;
+	};
+}
