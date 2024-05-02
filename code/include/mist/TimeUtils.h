db4e9d6cfa9693a1975bc8a7f5b16d82681e25cc
sergiodrm <sergiojg.drm@gmail.com>
2024-05-02 09:46:25 +0200

    time utils profile scope log

--- code/include/mist/TimeUtils.h
+++ code/include/mist/TimeUtils.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#define PROFILE_SCOPE_LOG(id, msg) Mist::tScopeProfiler __scopeprof_##id(msg)
 
 namespace Mist
 {
 	typedef long long tTimePoint;
 	tTimePoint GetTimePoint();
 	float GetMiliseconds(tTimePoint point);
+
+	class tScopeProfiler
+	{
+	public:
+		tScopeProfiler(const char* msg);
+		~tScopeProfiler();
+	private:
+		tTimePoint m_start;
+		char m_msg[128];
+	};
 }
