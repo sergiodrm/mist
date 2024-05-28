305102c61f451908854e82ba9db540766c8f9861
sergiodrm <sergiojg.drm@gmail.com>
2024-05-28 02:06:24 +0200

    RenderAPI

--- code/src/RenderAPI.cpp
+++ code/src/RenderAPI.cpp
@@ -1,8 +1,15 @@
 #include "RenderAPI.h"
+#include "Debug.h"
+
+namespace Mist_profiling
+{
+    extern sRenderStats GRenderStats;
+}
 
 namespace Mist
 {
-    namespace Render
+
+    namespace RenderAPI
     {
         bool WaitFences(Device device, const Fence* fences, uint32_t fenceCount, bool waitAll, uint32_t timeoutNs)
         {
@@ -13,6 +20,18 @@ namespace Mist
         void ResetFences(Device device, const Fence* fences, uint32_t fenceCount)
         {
             vkResetFences(device, fenceCount, fences);
+        }
+
+        void CmdDraw(CommandBuffer cmd, uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance)
+        {
+            vkCmdDraw(cmd, vertexCount, instanceCount, firstVertex, firstInstance);
+            ++Mist_profiling::GRenderStats.DrawCalls;
+        }
+
+        void CmdDrawIndexed(CommandBuffer cmd, uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, uint32_t vertexOffset, uint32_t firstInstance)
+        {
+            vkCmdDrawIndexed(cmd, indexCount, instanceCount, firstIndex, vertexOffset, firstInstance);
+            ++Mist_profiling::GRenderStats.DrawCalls;
         }
     }
 }
--- code/src/RenderAPI.h
+++ code/src/RenderAPI.h
@@ -4,14 +4,20 @@
 
 namespace Mist
 {
+
 	typedef VkFence Fence;
 	typedef VkDevice Device;
 	typedef VkCommandBuffer CommandBuffer;
-
-	namespace Render
+	typedef VkImageView ImageView;
+	typedef VkSampler Sampler;
+	namespace RenderAPI
 	{
+
 		bool WaitFences(Device device, const Fence* fences, uint32_t fenceCount, bool waitAll = true, uint32_t timeoutNs = 1000000000);
 		void ResetFences(Device device, const Fence* fences, uint32_t fenceCount);
+		void CmdDraw(CommandBuffer cmd, uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance);
+		void CmdDrawIndexed(CommandBuffer cmd, uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, uint32_t vertexOffset, uint32_t firstInstance);
+
 	}
 
 }
