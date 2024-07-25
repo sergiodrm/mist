88fa95f786aac845684651231222debbb9051e00
sergiodrm <sergiojg.drm@gmail.com>
2024-07-25 23:23:46 +0200

    log macros

--- code/include/mist/Core/Logger.h
+++ code/include/mist/Core/Logger.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define LOG_MSG_MAX_SIZE 1024*3
+#define LOG_MSG_MAX_SIZE 1024*10
 #define MSG_LENGTH LOG_MSG_MAX_SIZE
 #define MAX_LOG_COUNT 1024
 #define MAX_CMD_CALLBACKS_COUNT 64
@@ -56,3 +56,15 @@ namespace Mist
 		unsigned int m_cmdFunctionIndex;
 	};
 }
+
+#define loginfo(msg) logfinfo(msg)
+#define logdebug(msg) Mist::Log(Mist::LogLevel::Debug, msg)
+#define logok(msg) Mist::Log(Mist::LogLevel::Ok, msg)
+#define logerror(msg) logferror(msg)
+#define logwarn(msg) logfwarn(msg)
+
+#define logfinfo(msg, ...) Mist::Logf(Mist::LogLevel::Info, msg, __VA_ARGS__)
+#define logfdebug(msg, ...) Mist::Logf(Mist::LogLevel::Debug, msg, __VA_ARGS__)
+#define logfok(msg, ...) Mist::Logf(Mist::LogLevel::Ok, msg, __VA_ARGS__)
+#define logferror(msg, ...) Mist::Logf(Mist::LogLevel::Error, msg, __VA_ARGS__)
+#define logfwarn(msg, ...) Mist::Logf(Mist::LogLevel::Warn, msg, __VA_ARGS__)
