cdc7c01203a5152b131cad86497160c384893e1b
sergiodrm <sergiojg.drm@gmail.com>
2025-05-07 19:46:23 +0200

    profile log init, run and end

--- source/engine/Core/EntryPoint.cpp
+++ source/engine/Core/EntryPoint.cpp
@@ -5,11 +5,22 @@
 
 int main(int argc, char* argv[])
 {
-	Mist::InitSytemMemory();
-	Mist::InitLog("log.html");
-	Mist::tApplication* app = Mist::tApplication::CreateApplication(argc, argv);
-	int exitCode = app->Run();
-	Mist::tApplication::DestroyApplication(app);
+	Mist::tApplication* app;
+	int exitCode = 0;
+	{
+		PROFILE_SCOPE_LOG(InitApp, "Init app");
+		Mist::InitSytemMemory();
+		Mist::InitLog("log.html");
+		app = Mist::tApplication::CreateApplication(argc, argv);
+	}
+	{
+		PROFILE_SCOPE_LOG(RunApp, "Run app");
+		exitCode = app->Run();
+	}
+	{
+		PROFILE_SCOPE_LOG(InitApp, "Destroy app");
+		Mist::tApplication::DestroyApplication(app);
+	}
 	Mist::TerminateLog();
 	Mist::TerminateSystemMemory();
 	return exitCode;
