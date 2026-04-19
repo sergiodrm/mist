4be987d1e98f531ddf58e94cb31ece26e687accc
sergiodrm <sergiojg.drm@gmail.com>
2026-04-19 19:12:13 +0200

    Texture class to abstract logic texture from device texture. Implements new ResourceLoader in worker thread

---
+++ source/engine/Render/Texture.cpp
@@ -0,0 +1,83 @@
+#include "Texture.h"
+#include "RenderSystem/RenderSystem.h"
+#include "Utils/FileSystem.h"
+#include "Core/SystemMemory.h"
+
+#include <chrono>
+#include "Core/Logger.h"
+#include "VulkanRenderEngine.h"
+#include "RenderAPI/Device.h"
+
+namespace Mist
+{
+	Texture::Texture(const render::TextureHandle& deviceTexture)
+		: m_deviceTexture(deviceTexture)
+	{ }
+
+	Texture Texture::GetUnknownTexture()
+    {
+		Texture t;
+		return t;
+    }
+
+    bool Texture::LoadFromFile(const cAssetPath& filepath)
+    {
+		TextureLoader::LoadParams params;
+		params.filepath = filepath;
+		params.flipVertical = false;
+		params.calculateMipLevels = true;
+		TextureLoader* loader = _new TextureLoader(this, params);
+		resources::PushLoader(loader);
+		return true;
+    }
+
+	Texture::TextureLoader::TextureLoader(Texture* owner, const LoadParams& params)
+		: m_owner(owner), m_params(params), m_textureData{}
+	{
+		check(m_owner);
+		check(FileSystem::FileExists(m_params.filepath.c_str()));
+	}
+
+	resources::IResourceLoader::ProcType Texture::TextureLoader::GetProcType() const
+	{
+		return ProcType::LoadThread;
+	}
+
+	resources::IResourceLoader::ProcType Texture::TextureLoader::ProcessLoadThread()
+	{
+		if (!rendersystem::textureloader::LoadTextureData_u8(&m_textureData, m_params.filepath, m_params.flipVertical))
+		{
+			logferror("Failed loading texture from: %s\n", m_params.filepath);
+			return ProcType::Finished;
+		}
+		return ProcType::MainThread;
+	}
+
+	resources::IResourceLoader::ProcType Texture::TextureLoader::ProcessMainThread()
+	{
+		check(m_textureData.u8data);
+		check(m_textureData.width && m_textureData.height && m_textureData.depth);
+		render::TextureDescription desc;
+		desc.extent = { m_textureData.width, m_textureData.height, m_textureData.depth };
+		desc.format = render::Format_R8G8B8A8_UNorm;
+		desc.debugName = m_params.filepath;
+		desc.mipLevels = m_params.calculateMipLevels ? rendersystem::CalculateMipLevels(m_textureData.width, m_textureData.height) : 1;
+
+		// TODO: device as param
+		render::Device* device = g_device;
+		render::TextureHandle texture = device->CreateTexture(desc);
+
+		render::utils::UploadContext uploadContext(device);
+		uploadContext.WriteTexture(texture, 0, 0, m_textureData.u8data, m_textureData.width * m_textureData.height * sizeof(uint8_t) * m_textureData.channels);
+		uploadContext.SetTextureLayout(texture, render::ImageLayout_ShaderReadOnly, 0, 0);
+		if (m_params.calculateMipLevels)
+			rendersystem::GenerateMipMaps(device, texture, &uploadContext);
+
+		rendersystem::textureloader::FreeTextureData(m_textureData);
+
+		check(m_owner && !m_owner->m_deviceTexture);
+		m_owner->m_deviceTexture = texture;
+
+		return ProcType::Finished;
+	}
+}
---
+++ source/engine/Render/Texture.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "RenderAPI/Device.h"
+#include "RenderSystem/TextureLoader.h"
+#include "Core/Atomic.h"
+#include "Core/Thread.h"
+#include "Core/Mutex.h"
+
+#include "Utils/FileSystem.h"
+#include "Resources/ResourceLoader.h"
+
+namespace rendersystem
+{
+    class RenderSystem;
+}
+
+namespace Mist
+{
+    /**
+     * Wrapper for device texture. This class can process a load task in a worker thread meanwhile returns a default texture.
+     * Once the final texture is ready, the GetDeviceTexture() method will return the right texture loaded from the file.
+     */
+    class Texture
+    {
+    public:
+
+        class TextureLoader : public resources::IResourceLoader
+        {
+        public:
+
+            struct LoadParams
+            {
+                cAssetPath filepath;
+                bool flipVertical = false;
+                bool calculateMipLevels = true;
+            };
+
+            TextureLoader(Texture* owner, const LoadParams& params);
+            virtual ProcType GetProcType() const override;
+            virtual ProcType ProcessLoadThread() override;
+            virtual ProcType ProcessMainThread() override;
+        private:
+            Texture* m_owner;
+            LoadParams m_params;
+            rendersystem::textureloader::TextureData m_textureData;
+        };
+
+        Texture() = default;
+        Texture(const render::TextureHandle& deviceTexture);
+
+        static Texture GetUnknownTexture();
+
+        bool LoadFromFile(const cAssetPath& filepath);
+
+        const render::TextureHandle& GetDeviceTexture() const { return m_deviceTexture; }
+
+    private:
+        render::TextureHandle m_deviceTexture{ nullptr };
+    };
+}
