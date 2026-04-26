bc019a8b9b94e4e99fc5d4e4019cfcd4e9dfc572
sergiodrm <sergiojg.drm@gmail.com>
2026-04-26 18:41:31 +0200

    GBuffer_Zero shader function

--- assets/shaders/includes/gbuffer.glsl
+++ assets/shaders/includes/gbuffer.glsl
@@ -16,6 +16,20 @@ struct GBuffer
 	vec2 motionVectors;
 };
 
+GBuffer GBuffer_Zero()
+{
+	GBuffer gb;
+	gb.normal = vec3(0.f);
+	gb.roughness = 0.f;
+	gb.metallic = 0.f;
+	gb.emissive = vec3(0.f);
+	gb.opacity = 0.f;
+	gb.albedo = vec3(0.f);
+	gb.specular = 0.f;
+	gb.motionVectors = vec2(0.f);
+	return gb;
+}
+
 #ifdef CAMERA_DATA_INV_PROJECTION
 vec3 GBuffer_ReprojectPosition(vec2 texCoords, float depth)
 {
